82cf6c7bd865ba1558a24569423e1a2516e04cee
maxwellyue <756332309@qq.com>
2017-06-08 21:39:39 +0800

    添加jdk1.8.0_131 src

--- src/main/java/com/maxwell/readcode/collection/Read.java
+++ src/main/java/com/maxwell/readcode/collection/Read.java
@@ -20,6 +20,21 @@ public class Read {
     AbstractList abstractList = null;
     ArrayList arrayList = null;
 
+    public static void main(String[] args){
+        List<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add("8");
+        list.add("9");
+        for (Iterator i=list.iterator(); i.hasNext();){
+            System.out.println(i.next());
+        }
+    }
 
 
 
